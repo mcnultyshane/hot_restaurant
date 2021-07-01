084d46ee563ed765a7acb55777e2469fa86589b1
Keith Watford <keith.l.watford@gmail.com>
2021-07-01 11:11:18 -0400

    finished server(?)

--- server.js
+++ server.js
@@ -6,14 +6,30 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const reserve = [];
+
+const waitList = [];
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, './public/reseve.html')));
 app.get('/table', (req, res) => res.sendFile(path.join(__dirname, './public/table.html')));
 
-const reserve = [];
 
-const waitList = [];
+app.get('api/table', (req, res) => res.json)
+app.get('api/reserve', (req, res) => res.json) 
 
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
+app.post('api/reserve', (req, res) => {
+    const newReservation = req.body;
+
+    if (reserve.length + 1 <= 5) {
+        reserve.push(newReservation)
+    } else {
+        waitList.push(newReservation)
+    }
+});
+
+
+
+app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
